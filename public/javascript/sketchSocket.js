c6346c671e28008e1120921814d0575e15842184
japankid-code <japankid.jake@gmail.com>
2021-06-07 17:49:25 -0500

    update board controls

--- public/javascript/sketchSocket.js
+++ public/javascript/sketchSocket.js
@@ -34,7 +34,6 @@ function setup() {
 	// Getting our buttons and the holder through the p5.js dom
 	const color_picker = select('#pickcolor')
 	const color_btn = select('#color-btn')
-	const color_holder = select('#color-holder')
 
 	const stroke_width_picker = select('#stroke-width-picker')
 	const stroke_btn = select('#stroke-btn')
@@ -42,10 +41,8 @@ function setup() {
 	// Adding a mousePressed listener to the button
 	color_btn.mousePressed(() => {
 		// Checking if the input is a valid hex color
-		if (/(^[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
+		if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
 			color = color_picker.value()
-      color = "#" + color;
-			color_holder.style('background-color', color)
 		} else {
 			console.log('Enter a valid hex value')
 		}
