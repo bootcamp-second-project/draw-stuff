9b82fc7d52801eb8a02a60755420057e722a3266
japankid-code <japankid.jake@gmail.com>
2021-06-18 20:39:46 -0500

    :fire: homepage now works, leads to game page

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,11 +1,17 @@
 // select all relavant elements up here
+const usernameEl = document.getElementById(`usernameInput`)
+const playButtonEl = document.getElementById("playButton")
+const joinGameNumberEl = document.getElementById("gameNumber")
+const createNumRounds = document.getElementById(`numRound`)
+const createRoundTime = document.getElementById(`roundTime`)
+const createDrawList = document.getElementById(`drawList`)
+const dropdown = document.getElementById("gameOption")
+const createGameOptionsModal = document.getElementById('create-game-options')
+const joinGameOptionModal = document.getElementById('join-game-option')
 
-// homepage needs at least 3 fetches...
-
-const defaultUrl = "http://localhost:3001";
 
 // fetch function to post a new user to the database
-async function createUser(avatar, username, url = defaultUrl) {
+async function createUser(avatar, username) {
     const response = await fetch(`/api/users`, {
         method: "POST",
         body: JSON.stringify({
@@ -15,16 +21,14 @@ async function createUser(avatar, username, url = defaultUrl) {
         headers: {
             'Content-Type': 'application/json'
         }
-    });
-    const data = await response.json();
-    sessionStorage.setItem('user', JSON.stringify(data))
-    return data;
+    })
+    const user = await response.json()
+    sessionStorage.setItem('user', JSON.stringify(user))
+    return user
 }
 
-
-
 // fetch function to create a new game in the database
-async function createGame(rounds, roundTime, drawList, url = defaultUrl) {
+async function createGame(rounds, roundTime, drawList) {
     const response = await fetch(`/api/game`, {
         method: "POST",
         body: JSON.stringify({
@@ -35,14 +39,14 @@ async function createGame(rounds, roundTime, drawList, url = defaultUrl) {
         headers: {
             'Content-Type': 'application/json'
         }
-    });
-    const data = await response.json();
-    return data;
+    })
+    const data = await response.json()
+    return data
 }
 // fetch function to add a user as player of a game
 // this one should eventually redirect to '/play/:id' passing in the game id
 // user will end up drawing in a room with the other players in the game
-async function addUserToGame(game_id, user_id, url = defaultUrl) {
+async function addUserToGame(game_id, user_id) {
     const response = await fetch(`/api/player`, {
         method: "POST",
         body: JSON.stringify({
@@ -52,77 +56,90 @@ async function addUserToGame(game_id, user_id, url = defaultUrl) {
         headers: {
             'Content-Type': 'application/json'
         }
-    });
-    const data = await response.json();
-    return data;
+    })
+    const data = await response.json()
+    window.location = `/play/${data.id}`
+    return data
 }
 
-async function getAllGameIds(url = defaultUrl) {
+async function getAllGameIds() {
     const response = await fetch(`/api/game`, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json'
         }
-    });
-    return await response.json();
+    })
+    const data = await response.json();
+    const list = data.map((datum) => datum.id)
+    return list
 }
-
 
 // need some button handling that considers the selected dropdown value
 
-let gameId;
-const dropdown = document.getElementById("gameOption");
-let playFunction = async () => {
-    const numRounds = document.getElementById(`numRound`).value;
-    const roundTime = document.getElementById(`roundTime`).value;
-    const drawList = document.getElementById(`drawList`).value;
-    console.log(drawList);
-    await createGame(numRounds, roundTime);
-    window.location.href = `/play/${gameId}`;
-};
+let playFunction = async (type) => {
+  const username = usernameEl.value;
+  const availableGames = await getAllGameIds()
+  const user = await createUser(1, username)
+  
+  const friendsGame = joinGameNumberEl.value
+
+  const numRounds = createNumRounds.value;
+  const roundTime = createRoundTime.value;
+  if (drawList.textContent) {
+    let drawPhrases = drawList.textContent.split(' ')
+    let phraseObj = { "phrases": drawList }
+  }
+
+  switch(type) {
+    case 'random':
+      const randomGame = availableGames[Math.floor(Math.random() * availableGames.length)]
+      await addUserToGame(randomGame, user.id)
+      window.location.href = `/play/${randomGame}`
+      break
+    case 'join':
+      addUserToGame(friendsGame, user.id)
+      window.location = `/play/${friendsGame}`
+      break
+    case 'create':
+      let createdGame = await createGame(numRounds, roundTime, [phraseObj])
+      addUserToGame(createdGame.id, user.id)
+      window.location = `/play/${game.id}`
+      break
+      }
+}
+
+
 // addEventListener(event, function);
 dropdown.addEventListener("change", async (event) => {
-    // create | join | random
-    const selectedDropdown = event.target.value;
-    if (selectedDropdown === "create") {
-        // ask for rounds, round time, and draw list
-        playFunction = async () => {
-            const numRounds = document.getElementById(`numRound`).value;
-            const roundTime = document.getElementById(`roundTime`).value;
-            const drawList = document.getElementById(`drawList`).value.split(new RegExp(",\\s*"));
-            await createGame(numRounds, roundTime, drawList);
-            window.location.href = `/play/${gameId}`;
-        };
-        document.getElementById("playButton").textContent = "Create Game";
-
-    } else if (selectedDropdown === "join") {
-        // i think we need to grab a game by its id from the input
-        playFunction = async () => {
-            const gameId = document.getElementById(`gameNumber`).value;
-            const usernameText = document.getElementById(`usernameInput`).value;
-            const newUser = await createUser(0, usernameText);
-            await addUserToGame(gameId, newUser.id);
-            window.location.href = `/play/${gameId}`;
-        }
-        document.getElementById("playButton").textContent = "Play";
-
-    } else if (selectedDropdown === "random") {
-        // grab a random game id from server
-        const gameIds = await getAllGameIds();
-        const randomIndex = Math.floor(Math.random() * gameIds.length);
-        gameId = gameIds[randomIndex];
-        playFunction = async () => {
-            const usernameText = document.getElementById(`usernameInput`).value;
-            const newUser = await createUser(0, usernameText);
-            await addUserToGame(gameId, newUser.id);
-            window.location.href = `/play/${gameId}`;
-        }
-        document.getElementById("playButton").textContent = "Play";
-    }
+  // create | join | random
+  
+  const selectedDropdown = event.target.value;
+  if (selectedDropdown === "create") {
+    joinGameOptionModal.classList.add('hidden')
+    createGameOptionsModal.classList.remove('hidden')
+    playButtonEl.textContent = "Create Game!"
+  } else if (selectedDropdown === "join") {
+    // join friend's game hide create game
+    createGameOptionsModal.classList.add('hidden')
+    joinGameOptionModal.classList.remove('hidden')
+    playButtonEl.textContent = "Play!"
+  } else if (selectedDropdown === "random") {
+    joinGameOptionModal.classList.add('hidden')
+    createGameOptionsModal.classList.add('hidden')
+    playButtonEl.textContent = "Play!"
+  }
 });
 
-document.getElementById("playButton").onclick = async () => {
-    playFunction();
+document.getElementById("playButton").onclick = async (e) => {
+  e.preventDefault()
+  const selectedDropdown = dropdown.value
+  if (selectedDropdown === "create") {
+    playFunction("create")
+  } else if (selectedDropdown === "join") {
+    playFunction("join")
+  } else if (selectedDropdown === "random") {
+    playFunction("random")
+  }
 }
 
 // disable the play button while creating a game is selected
